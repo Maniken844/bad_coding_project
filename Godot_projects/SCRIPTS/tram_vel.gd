bb0c3336a6949644ef3025c8b2fa2a0680aff7b1
mnkn844 <43299939+Maniken844@users.noreply.github.com>
2023-07-30 04:15:51 +0300

    0.3.6
    -trampolines works PERFECT now
    - added ability to change power of each trampoline from editor

---
+++ Godot_projects/SCRIPTS/tram_vel.gd
@@ -0,0 +1,13 @@
+extends Node3D
+@onready var csg_cylinder_3d = $Cylinder/CSGCylinder3D
+
+@export var vel: float = 10.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	csg_cylinder_3d.velocity = vel
+	#print(csg_cylinder_3d.vel)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- Godot_projects/SCRIPTS/trampoline.gd
+++ Godot_projects/SCRIPTS/trampoline.gd
@@ -1,22 +1,18 @@
 extends CSGCylinder3D
 
-var Player_going_up = false
 var Player
-var Player_highest_value = 0
 
 @onready var animation_player = $"../../AnimationPlayer"
+@onready var trampoline = $"../.."
+
+@export var velocity: float
 
 
-
-
+func _ready():
+	pass
 func _physics_process(delta):
-	if Player_going_up:
-		Player.position.y += GlobalVariables.player_gravity * 1.3 *delta
-		if Player.position.y > Player_highest_value:
-			Player_highest_value = Player.position.y
-		elif Player.position.y < Player_highest_value:
-			Player_going_up = false
-			Player_highest_value = 0
+	pass
+	
 
 
 
@@ -24,5 +20,5 @@ func _physics_process(delta):
 func _on_area_3d_body_entered(body):
 	if body.name == "Player":
 		Player = body
-		Player_going_up = true
+		Player.velocity.y = velocity
 		animation_player.play("tram_jump")
